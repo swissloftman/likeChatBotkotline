4d58f1ccbb247ca89cf6d96f4594ac07967159d5
swissloftman <swissloftman@gmail.com>
2018-06-18 23:11:41 +0200

    add like and dislike ability add paho client

--- src/main/kotlin/org/codingfarm/worklshop/AxaLikeBot.kt
+++ src/main/kotlin/org/codingfarm/worklshop/AxaLikeBot.kt
@@ -6,11 +6,48 @@ import org.telegram.abilitybots.api.util.AbilityUtils.getChatId
 import org.telegram.telegrambots.api.objects.Update
 import java.util.function.Consumer
 import java.util.function.Predicate
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttMessage
 
 class AxaLikeBot(botUserName: String, botToken: String) : AbilityBot(botUserName,botToken) {
 
+    private var client:MqttClient;
+
+    init {
+        client = MqttClient("tcp://iot.eclipse.org:1883", "0815")
+        client.connect()
+    }
+
+
     override fun creatorId(): Int {
         return 580924163
+    }
+
+
+
+
+    @Suppress("unused")
+    fun like(): Ability {
+        return Ability
+                .builder()
+                .name("like")
+                .info("send a like")
+                .locality(Locality.ALL)
+                .privacy(Privacy.PUBLIC)
+                .action {context -> client.publish("paho/metest/axadodavote", MqttMessage("like".toByteArray())); silent.send("Danke für den Like", context.chatId())}
+                .build()
+    }
+
+    @Suppress("unused")
+    fun dislike(): Ability {
+        return Ability
+                .builder()
+                .name("dislike")
+                .info("send a dislike")
+                .locality(Locality.ALL)
+                .privacy(Privacy.PUBLIC)
+                .action {context -> client.publish("paho/metest/axadodavote", MqttMessage("dislike".toByteArray())); silent.send("schade ... ein dislike", context.chatId())}
+                .build()
     }
 
     @Suppress("unused")
